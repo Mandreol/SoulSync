67607dc0a0bee7d8c48c6fedfb4cd553a207470e
Mandreol <marcoandreola01@gmail.com>
2023-06-13 21:48:05 -0300

    egreagado y configuracion de nuevos mazos

--- src/components/DeckCard.jsx
+++ src/components/DeckCard.jsx
@@ -4,14 +4,20 @@ import { TransitionGroup } from "react-transition-group";
 
 const DeckCard = () => {
 	let deck = [];
-	const n = 6;
-	const mazo = 2;
+	const n = 3;
+	const mazo = 3;
+	const texts = ["Pasado", "Presente", "Futuro"];
+	let tiradaClass = "";
 
 	for (let i = 1; i <= n; i++) {
 		deck.push({ id: i, mazo });
 	}
+	deck.map((e, i) => (e.text = texts[i]));
+
+	tiradaClass =
+		n <= 8 ? { gap: "5rem", height: "100vh" } : { gap: "0.5rem", height: "100%" };
 	return (
-		<TransitionGroup className="deckCard">
+		<TransitionGroup className="deckCard" style={tiradaClass}>
 			{deck.map((c) => (
 				<FlipabbeCard key={c.id} deck={c} />
 			))}
--- src/components/FlipabbeCard.jsx
+++ src/components/FlipabbeCard.jsx
@@ -1,23 +1,37 @@
+import { useState } from "react";
 import Card from "./Card";
 import { CSSTransition } from "react-transition-group";
-import { useState } from "react";
 import "../styles/flipabbleCard.css";
 
-const FlipabbeCard = (deck) => {
+const FlipabbleCard = (deck) => {
 	const [showFront, setShowFront] = useState(true);
+	const [showCard, setShowCard] = useState(false);
+
+	const handleOnClick = (e) => {
+		e.stopPropagation();
+		setShowFront((prevState) => !prevState);
+	};
+
+	const handleDoubleClick = (e) => {
+		setShowCard((prevState) => !prevState);
+	};
+
+	const showClass = showCard ? "showCard" : "card";
+	const flippableClass = `flipabble-card-container-maso_${deck.deck.mazo}`;
+	console.log(flippableClass);
 	return (
-		<div className="flipabble-card-container">
+		<div className={flippableClass}>
 			<CSSTransition in={showFront} timeout={300} classNames="flip">
 				<Card
 					key={deck.deck.id}
-					onClick={() => {
-						setShowFront((v) => !v);
-					}}
+					onClick={handleOnClick}
 					deck={deck}
+					onDoubleClick={handleDoubleClick}
+					showClass={showClass}
 				/>
 			</CSSTransition>
 		</div>
 	);
 };
 
-export default FlipabbeCard;
+export default FlipabbleCard;
